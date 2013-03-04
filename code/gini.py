5a368f18bc6dd00a0070203d5b736f7168616c96
Joanna Hsu <joannahsu@ucbvpn-209-207.vpn.berkeley.edu>
2013-03-03 21:00:36 -0800

    first complete attempt at gini

--- code/gini.py
+++ code/gini.py
@@ -10,8 +10,31 @@ import csv
 contbr_employer, contbr_occupation, contb_receipt_amt, contb_receipt_dt,
 receipt_desc, memo_cd, memo_text, form_tp, file_num, tran_id, election_tp) = range(18)
 
+##PART ONE: OVERALL GINI 
+denom=0
+CC={}  ##dictionary for candidate counter
+ginipieces = [] ##create list for the individual pieces of Gini Index
+for row in csv.reader(fileinput.input()):
+    if not fileinput.isfirstline():
+	        if not row[cand_nm] in CC:
+	        	CC[row[cand_nm]]=1
+	        else:
+	        	CC[row[cand_nm]]+=1
+	        denom+=1
+
+for key in CC:
+	ginipieces.append((CC[key]/float(denom))**2) # current Gini Index using candidate name as the class
+
+gini =  1-sum(ginipieces) # current Gini Index using candidate name as the class, .2914 for entire data set
+print "Gini Index: %s" % gini
+
+
+
+
+####PREPARE FOR QUESTION TWO, by zipcode
 ZIPS={}
 totaldonors=0
+ginipieces = [] ##create list for the individual pieces of Gini Index
 csv.reader(fileinput.input()):
 	    if not fileinput.isfirstline():
 	    	if not row[contr_zip] in ZIPS:
@@ -20,40 +43,25 @@ csv.reader(fileinput.input()):
 				ZIPS[row[contbr_zip]].append(row)
 			totaldonors+=1
 
-def Gini (list_of_rows):
-############### Set up variables
-# TODO: declare datastructures
+def Gini (list_of_rows): #create function we can call
 	denom=0
 	CC={}  ##dictionary for candidate counter
-	############### Read through files
 	for row in list_of_rows
 	        if not row[cand_nm] in CC:
 	        	CC[row[cand_nm]]=1
 	        else:
 	        	CC[row[cand_nm]]+=1
 	        denom+=1
-
-	###
-	# TODO: calculate the values below:
-
-	ginipieces = [] ##create list for the individual pieces of Gini Index
+	
 	for key in CC:
 		ginipieces.append((CC[key]/float(denom))**2) # current Gini Index using candidate name as the class
-	return =  1-sum(ginipieces) #.2914 for entire data set
+	return =  1-sum(ginipieces)
 
-####################DO EVERYTHING
-
-Gini(alldata)
+####################DO PART TWO
 
 ZipGinis=[] #create list to hold Ginis for all zip codes
-
 for key in ZIPS:
 	ZipGinis.append(Gini(ZIPS[key])*len(ZIPS[key])/totaldonors) #Call our function, have it iterate over each dictionary key
 	
-
 split_gini = sum(ZipGinis) # weighted average of the Gini Indexes using candidate names, split up by zip code
-##/
-
-
-print "Gini Index: %s" % gini
 print "Gini Index after split: %s" % split_gini
