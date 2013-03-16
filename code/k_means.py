ae24e321a0308d9259d57a5122b19834e7f41e3f
Joanna Hsu <joannahsu@Joannas-MacBook-Pro.local>
2013-03-16 12:19:05 -0700

    hw kmeans

--- code/k_means.py
+++ code/k_means.py
@@ -2,7 +2,8 @@
 # Implement simple k-means clustering using 1 dimensional data
 # Joanna's changes
 ##/
-
+import random
+from random import *
 dataset = [-13.65089255716321, -0.5409562932238607, -88.4726466247223, 39.30158828358612, 4.066458182574449, 64.64143300482378, 38.68269424751338, 33.42013676314311, 31.18603331719732, -0.2027616409406292, 45.13590038987272, 30.791899783552395, 61.1727490302448, 18.167220741624856, 88.88077709786394, -1.3808002119514704, 50.14991362212521, 55.92029956281276, -6.759813255299466, 34.28290084421072]
 k = 2 # number of clusters
 
@@ -15,28 +16,28 @@ def pick_centroids(xs, num):
     """Return list of num centroids given a list of numbers in xs"""
     ###
     # TODO select and return centroids
-    return [1,2]
+    return sample(xs, num)
     ##/
 
 def distance(a, b):
     """Return the distance of numbers a and b"""
     ###
     # TODO return correct expression
-    return 0
+    return abs(a-b)
     ##/
 
 def centroid(xs):
     """Return the centroid number given a list of numbers, xs"""
     ###
     # TODO calculate and return centroid
-    return 0
+    return float(sum(xs))/len(xs)
     ##/
 
 def cluster(xs, centroids):
     """Return a list of clusters centered around the given centroids.  Clusters
     are lists of numbers."""
 
-    clusters = [[] for c in centroids]
+    clusters = [[] for c in centroids] ##creates list of list for centroids
 
     for x in xs:
         # find the closest cluster to x
